d8a3451cd40e23361e561e19b88b4f105b98c21a
EmilianoV-TEC <a01707035@itesm.mx>
2022-09-07 15:46:11 -0800

    Agrega view sign up

---
+++ app/src/main/java/com/example/qulturapp/view/sesion/ActivitySignUp.kt
@@ -0,0 +1,13 @@
+package com.example.qulturapp.view.sesion
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.qulturapp.R
+
+class ActivitySignUp: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}

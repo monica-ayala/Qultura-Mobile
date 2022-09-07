f96ed53c84415ac8120f969ac0e1bdcc88188fb1
EmilianoV-TEC <a01707035@itesm.mx>
2022-09-07 15:58:11 -0800

    Agrega view sign in

--- app/src/main/java/com/example/qulturapp/view/MainActivity.kt
+++ app/src/main/java/com/example/qulturapp/view/MainActivity.kt
@@ -4,14 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.qulturapp.R
-import com.example.qulturapp.view.sesion.ActivitySignUp
+import com.example.qulturapp.view.sesion.ActivitySignIn
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intentSignUp = Intent(this, ActivitySignUp::class.java)
-        startActivity(intentSignUp)
+        val intentSignIn = Intent(this, ActivitySignIn::class.java)
+        startActivity(intentSignIn)
     }
 }

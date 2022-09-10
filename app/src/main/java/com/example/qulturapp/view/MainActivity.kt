fba06ea53f5621fd23b58a4238603ad2e4326d4a
juanManuelGonzalezA00572003 <85131308+juanManuelGonzalezA00572003@users.noreply.github.com>
2022-09-10 12:35:18 -0500

    Event view done

--- app/src/main/java/com/example/qulturapp/view/MainActivity.kt
+++ app/src/main/java/com/example/qulturapp/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.qulturapp.R
+import com.example.qulturapp.view.eventos.EventoActivity
 import com.example.qulturapp.view.sesion.ActivitySignIn
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intentSignIn = Intent(this, ActivitySignIn::class.java)
-        startActivity(intentSignIn)
+        val intentEvento = Intent(this, EventoActivity::class.java)
+        startActivity(intentEvento)
     }
 }

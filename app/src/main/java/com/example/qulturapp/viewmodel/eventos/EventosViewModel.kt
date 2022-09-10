1485770ec7a30934fb6a5a847855aa9bfbb3da25
juanManuelGonzalezA00572003 <85131308+juanManuelGonzalezA00572003@users.noreply.github.com>
2022-09-10 12:53:30 -0500

    Ya se ven los eventos

--- app/src/main/java/com/example/qulturapp/viewmodel/eventos/EventosViewModel.kt
+++ app/src/main/java/com/example/qulturapp/viewmodel/eventos/EventosViewModel.kt
@@ -7,11 +7,18 @@ class EventosViewModel: ViewModel() {
 
     val listaEventos = mutableListOf<Evento>()
 
+
     fun agregarEvento (){
         val evento1 = Evento(1, "Evento de rock en la plaza de armas", "18/10/22", "https://amqueretaro.com/queretaro/2021/03/28/anuncian-evento-virtual-la-cultura-popular-e-indigena-de-queretaro-2021/", "Rock en querataro")
         val evento2 = Evento(2, "Evento de rock en la plaza de armas", "18/10/22", "https://amqueretaro.com/queretaro/2021/03/28/anuncian-evento-virtual-la-cultura-popular-e-indigena-de-queretaro-2021/", "Rock en querataro")
         val evento3 = Evento(3, "Evento de rock en la plaza de armas", "18/10/22", "https://amqueretaro.com/queretaro/2021/03/28/anuncian-evento-virtual-la-cultura-popular-e-indigena-de-queretaro-2021/", "Rock en querataro")
-
+        listaEventos.add(evento1)
+        listaEventos.add(evento2)
+        listaEventos.add(evento3)
     }
+
+
+
+
 
 }

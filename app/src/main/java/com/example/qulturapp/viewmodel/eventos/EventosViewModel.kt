3392e2f13bc745edc46036bc058a55ba2bbbb49e
juanManuelGonzalezA00572003 <85131308+juanManuelGonzalezA00572003@users.noreply.github.com>
2022-09-22 18:33:29 -0500

    before merge, wish luck

--- app/src/main/java/com/example/qulturapp/viewmodel/eventos/EventosViewModel.kt
+++ app/src/main/java/com/example/qulturapp/viewmodel/eventos/EventosViewModel.kt
@@ -10,15 +10,10 @@ class EventosViewModel: ViewModel() {
 
     fun agregarEvento (){
         val evento1 = Evento(1, "Evento de rock en la plaza de armas", "18/10/22", "https://amqueretaro.com/queretaro/2021/03/28/anuncian-evento-virtual-la-cultura-popular-e-indigena-de-queretaro-2021/", "Rock en querataro")
-        val evento2 = Evento(2, "Evento de rock en la plaza de armas", "18/10/22", "https://amqueretaro.com/queretaro/2021/03/28/anuncian-evento-virtual-la-cultura-popular-e-indigena-de-queretaro-2021/", "Rock en querataro")
-        val evento3 = Evento(3, "Evento de rock en la plaza de armas", "18/10/22", "https://amqueretaro.com/queretaro/2021/03/28/anuncian-evento-virtual-la-cultura-popular-e-indigena-de-queretaro-2021/", "Rock en querataro")
+        val evento2 = Evento(2, "Sanctorum", "18/10/22", "https://pbs.twimg.com/media/FcodWemXoAEXIHF.jpg", "Sanctorum")
+        val evento3 = Evento(3, "Cata de vino", "18/10/22", "https://billetto.co.uk/blog/wp-content/uploads/2019/05/kym-ellis-391585-unsplash-1024x683.jpg", "Catta de vinos")
         listaEventos.add(evento1)
         listaEventos.add(evento2)
         listaEventos.add(evento3)
     }
-
-
-
-
-
 }

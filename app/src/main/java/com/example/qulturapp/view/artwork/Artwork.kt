6f098b9f9753a5de48f50af198c5f81d349dd0eb
LeilaelRico <A01707023@tec.mx>
2022-09-23 01:21:42 -0500

    Artwork activity class added

---
+++ app/src/main/java/com/example/qulturapp/view/artwork/Artwork.kt
@@ -0,0 +1,12 @@
+package com.example.qulturapp.view.artwork
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.qulturapp.R
+
+class Artwork: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artwork)
+    }
+}

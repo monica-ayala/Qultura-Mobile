996b1cefe895babde1900e5ece6eb2152b085d95
juanManuelGonzalezA00572003 <85131308+juanManuelGonzalezA00572003@users.noreply.github.com>
2022-09-23 14:10:50 -0500

    bton sirve

--- app/src/main/java/com/example/qulturapp/view/eventos/EventoActivity.kt
+++ app/src/main/java/com/example/qulturapp/view/eventos/EventoActivity.kt
@@ -1,6 +1,10 @@
 package com.example.qulturapp.view.eventos
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Gallery
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -26,8 +30,18 @@ class EventoActivity: AppCompatActivity() {
 
     }*/
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_consulta_eventos)
+
+        val btn = findViewById<View>(R.id.Boton_evento)
+        btn.setOnClickListener{
+            val intent = Intent(this, EventoDetalle::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }

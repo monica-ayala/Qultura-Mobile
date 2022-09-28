bae1b1dcc7380f0554b1416cad05241f4110047c
saidortigoza <a01707430@itesm.mx>
2022-09-28 12:07:29 -0500

    Created phan12 branch

--- app/src/main/java/com/example/qulturapp/view/configuracion/ActivityConfiguration.kt
+++ app/src/main/java/com/example/qulturapp/view/configuracion/ActivityConfiguration.kt
@@ -20,31 +20,27 @@ class ActivityConfiguration : AppCompatActivity() {
         setContentView(R.layout.activity_configuration)
 
         // Dark Mode Function
-        var dm_switch = binding.darkmodeSwitch
-        val appSettingPrefs : SharedPreferences = getSharedPreferences("AppSettingsPrefs", 0)
-        val sharedPrefEdit : SharedPreferences.Editor = appSettingPrefs.edit()
-        val isNightModeOn : Boolean = appSettingPrefs.getBoolean("NightMode", false)
-
-        if(isNightModeOn) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        }
-
-        dm_switch.setOnCheckedChangeListener { _, isChecked ->
-            if(isNightModeOn) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                sharedPrefEdit.putBoolean("NightMode", false)
-                sharedPrefEdit.apply()
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                sharedPrefEdit.putBoolean("NightMode", true)
-                sharedPrefEdit.apply()
-            }
-        }
-
-        binding.profileAccessButton.setOnClickListener() {
-            Toast.makeText(this, "Hola", Toast.LENGTH_SHORT).show()
-        }
+//        var dm_switch = binding.darkmodeSwitch
+//        val appSettingPrefs : SharedPreferences = getSharedPreferences("AppSettingsPrefs", 0)
+//        val sharedPrefEdit : SharedPreferences.Editor = appSettingPrefs.edit()
+//        val isNightModeOn : Boolean = appSettingPrefs.getBoolean("NightMode", false)
+//
+//        if(isNightModeOn) {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//        } else {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+//        }
+//
+//        dm_switch.setOnCheckedChangeListener { _, isChecked ->
+//            if(isNightModeOn) {
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+//                sharedPrefEdit.putBoolean("NightMode", false)
+//                sharedPrefEdit.apply()
+//            } else {
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//                sharedPrefEdit.putBoolean("NightMode", true)
+//                sharedPrefEdit.apply()
+//            }
+//        }
     }
 }

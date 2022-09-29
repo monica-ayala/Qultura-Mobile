5de2215095daf692e929f8e3ab6147ba3957e3ea
Caceres-A01706972 <A01706972@tec.mx>
2022-09-29 12:12:37 -0500

    Adds a comment

--- app/src/main/java/com/example/qulturapp/model/Info/Guia.kt
+++ app/src/main/java/com/example/qulturapp/model/Info/Guia.kt
@@ -1,7 +1,7 @@
 package com.example.qulturapp.model.Info
 
 import com.google.gson.annotations.SerializedName
-
+//Comment
 data class Guia(
     @SerializedName("name") val name: String,
     @SerializedName("description") val desc: String,

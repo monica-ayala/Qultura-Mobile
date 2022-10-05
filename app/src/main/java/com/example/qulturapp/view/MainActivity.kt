487944e34404190c490f540d7b2f55d18bf7a004
LeilaelRico <A01707023@tec.mx>
2022-10-05 12:28:29 -0500

    Initial MediaPlayer Implementation

--- app/src/main/java/com/example/qulturapp/view/MainActivity.kt
+++ app/src/main/java/com/example/qulturapp/view/MainActivity.kt
@@ -3,31 +3,19 @@ package com.example.qulturapp.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.qulturapp.R
 import com.example.qulturapp.view.museum.ListMuseum
-import com.example.qulturapp.view.museum.Museum
-import com.example.qulturapp.model.museums.MuseumListAdapter
-import com.example.qulturapp.model.museums.MuseumResults
-//import com.example.qulturapp.view.eventos.EventoActivity
-import com.example.qulturapp.view.lounge.Lounge
-import com.example.qulturapp.view.Info.ActivityInfo
-import com.example.qulturapp.view.configuracion.ActivityConfiguration
-import com.example.qulturapp.view.emergencia.EmergenciaActivity
-import com.example.qulturapp.view.mapas.ActivityMap
-import com.example.qulturapp.view.perfil.ProfileActivity
-import com.example.qulturapp.view.sesion.ActivitySignIn
-import com.example.qulturapp.view.solicitudes.ActivityHorario
-import com.example.qulturapp.view.sesion.ActivitySignUp
-import com.example.qulturapp.viewmodel.museums.MuseumsViewModel
-import com.example.qulturapp.view.solicitudes.ActivitySolicitudes
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val intentSignIn = Intent(this, Lounge::class.java)
+        val intentSignIn = Intent(this, ListMuseum::class.java)
         startActivity(intentSignIn)
+
     }
 }
--- app/src/main/java/com/example/qulturapp/view/artwork/Artwork.kt
+++ app/src/main/java/com/example/qulturapp/view/artwork/Artwork.kt
@@ -9,9 +9,21 @@ import com.example.qulturapp.model.artwork.ArtworkResults
 import com.example.qulturapp.view.museum.Lounge
 import com.example.qulturapp.viewmodel.artworks.ArtworkListAdapter
 import com.example.qulturapp.viewmodel.artworks.ArtworkViewModel
+import android.media.AudioManager
+import android.media.ImageReader
+import android.media.MediaPlayer
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 
-class Artwork: AppCompatActivity() {
-    private lateinit var artList:List<ArtworkResults>
+class Artwork : AppCompatActivity() {
+
+    lateinit var playIB: ImageButton
+    lateinit var pauseIB: ImageButton
+    lateinit var mediaPlayer: MediaPlayer
+    //lateinit var play_pause: ImageView
+
+    private lateinit var artList: List<ArtworkResults>
 
     private lateinit var adapter: ArtworkListAdapter
 
@@ -19,10 +31,75 @@ class Artwork: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lounge)
+        setContentView(R.layout.activity_artwork)
 
-        val intentSignIn = Intent(this, Lounge::class.java)
-        startActivity(intentSignIn)
-        viewmodel.getObra()
+        //viewmodel.getObra()
+
+        playIB = findViewById(R.id.idIBPlay)
+        pauseIB = findViewById(R.id.idIBPause)
+
+        // on below line we are
+        // initializing our media player
+        mediaPlayer = MediaPlayer()
+
+        playIB.setOnClickListener {
+
+            // on below line we are creating a variable for our audio url
+            var audioUrl = "https://qulturaqro.live/uploads/1.01%20Discovery%20of%20Gravitation.mp3"
+            // on below line we are setting audio stream
+            // type as stream music on below line.
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+
+            // on below line we are running a try
+            // and catch block for our media player.
+            try {
+                // on below line we are setting audio
+                // source as audio url on below line.
+                mediaPlayer.setDataSource(audioUrl)
+
+                // on below line we are
+                // preparing our media player.
+                mediaPlayer.prepare()
+
+                // on below line we are
+                // starting our media player.
+                mediaPlayer.start()
+
+            } catch (e: Exception) {
+
+                // on below line we are handling our exception.
+                e.printStackTrace()
+            }
+            // on below line we are displaying a toast message as audio player.
+            Toast.makeText(applicationContext, "Audio started playing..", Toast.LENGTH_SHORT).show()
+
+        }
+
+        pauseIB.setOnClickListener {
+            // on below line we are checking
+            // if media player is playing.
+            if (mediaPlayer.isPlaying) {
+                // if media player is playing we
+                // are stopping it on below line.
+                mediaPlayer.stop()
+
+                // on below line we are resetting
+                // our media player.
+                mediaPlayer.reset()
+
+                // on below line we are calling
+                // release to release our media player.
+                mediaPlayer.release()
+
+                // on below line we are displaying a toast message to pause audio/
+                Toast.makeText(applicationContext, "Audio has been  paused..", Toast.LENGTH_SHORT)
+                    .show()
+
+            } else {
+                // if audio player is not displaying we are displaying below toast message
+                Toast.makeText(applicationContext, "Audio not played..", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }

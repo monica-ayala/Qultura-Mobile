96cb259b4853484e6af9624743e34debb0bd41db
LeilaelRico <A01707023@tec.mx>
2022-10-05 21:10:31 -0500

    MediaPlayer Visual Change / Drawables changed

--- app/src/main/java/com/example/qulturapp/view/artwork/Artwork.kt
+++ app/src/main/java/com/example/qulturapp/view/artwork/Artwork.kt
@@ -38,66 +38,41 @@ class Artwork : AppCompatActivity() {
         playIB = findViewById(R.id.idIBPlay)
         pauseIB = findViewById(R.id.idIBPause)
 
-        // on below line we are
-        // initializing our media player
         mediaPlayer = MediaPlayer()
 
         playIB.setOnClickListener {
+                var audioUrl = "https://qulturaqro.live/uploads/1.01%20Discovery%20of%20Gravitation.mp3"
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
 
-            // on below line we are creating a variable for our audio url
-            var audioUrl = "https://qulturaqro.live/uploads/1.01%20Discovery%20of%20Gravitation.mp3"
-            // on below line we are setting audio stream
-            // type as stream music on below line.
-            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+                try {
+                    mediaPlayer.setDataSource(audioUrl)
 
-            // on below line we are running a try
-            // and catch block for our media player.
-            try {
-                // on below line we are setting audio
-                // source as audio url on below line.
-                mediaPlayer.setDataSource(audioUrl)
+                    mediaPlayer.prepare()
 
-                // on below line we are
-                // preparing our media player.
-                mediaPlayer.prepare()
+                    mediaPlayer.start()
 
-                // on below line we are
-                // starting our media player.
-                mediaPlayer.start()
+                } catch (e: Exception) {
 
-            } catch (e: Exception) {
-
-                // on below line we are handling our exception.
-                e.printStackTrace()
-            }
-            // on below line we are displaying a toast message as audio player.
-            Toast.makeText(applicationContext, "Audio started playing..", Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+                Toast.makeText(applicationContext, "Comienza Reproducción", Toast.LENGTH_SHORT).show()
 
         }
 
         pauseIB.setOnClickListener {
-            // on below line we are checking
-            // if media player is playing.
             if (mediaPlayer.isPlaying) {
-                // if media player is playing we
-                // are stopping it on below line.
+
                 mediaPlayer.stop()
 
-                // on below line we are resetting
-                // our media player.
                 mediaPlayer.reset()
 
-                // on below line we are calling
-                // release to release our media player.
                 mediaPlayer.release()
 
-                // on below line we are displaying a toast message to pause audio/
-                Toast.makeText(applicationContext, "Audio has been  paused..", Toast.LENGTH_SHORT)
+                Toast.makeText(applicationContext, "Audio se ha reiniciado", Toast.LENGTH_SHORT)
                     .show()
 
             } else {
-                // if audio player is not displaying we are displaying below toast message
-                Toast.makeText(applicationContext, "Audio not played..", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "No hay audio reproduciéndose", Toast.LENGTH_SHORT).show()
             }
 
         }

5c839647a25b287a0964baf91d9b01612fedc9a9
AudibleParrot2 <A01703259@tec.mx>
2022-10-07 11:57:13 -0500

    Nav with DB

--- app/src/main/java/com/example/qulturapp/viewmodel/artworks/ArtworkListAdapter.kt
+++ app/src/main/java/com/example/qulturapp/viewmodel/artworks/ArtworkListAdapter.kt
@@ -1,15 +1,20 @@
 package com.example.qulturapp.viewmodel.artworks
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.qulturapp.R
 import com.example.qulturapp.databinding.ItemMuseumDetailBinding
 import com.example.qulturapp.databinding.ItemObrasBinding
 import com.example.qulturapp.model.artwork.ArtworkResults
+import com.example.qulturapp.view.artwork.Artwork
+import com.example.qulturapp.view.lounge.Lounge
+import com.squareup.picasso.Picasso
 
 
 class ArtworkListAdapter(private val data: List<ArtworkResults>, private val context: Context) : RecyclerView.Adapter<ViewHolder>(){
@@ -25,6 +30,15 @@ class ArtworkListAdapter(private val data: List<ArtworkResults>, private val con
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = dataArtworkML[position]
         holder.bind(item)
+
+        holder.obraImg.setOnClickListener {
+            val intent = Intent(context, Artwork::class.java)
+            intent.putExtra("nom", item.name)
+            intent.putExtra("url", item.url)
+            intent.putExtra("id",item.id_obra)
+
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int = dataArtworkML.size
@@ -33,11 +47,15 @@ class ArtworkListAdapter(private val data: List<ArtworkResults>, private val con
 
 class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val obraNombre = view.findViewById(R.id.tvObraNom) as TextView
+    val obraImg = view.findViewById(R.id.ivObra) as ImageView
 
     val context = view.context
 
     fun bind(item: ArtworkResults) {
         obraNombre.text = item.name
+
+        val ligaImg = "https://qulturaqro.live/uploads/" + item.url
+        Picasso.get().load(ligaImg).into(obraImg);
     }
 }
 

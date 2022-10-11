af5bc44b927a0a50edc876b6c655110636f0f34b
EmilianoV-TEC <a01707035@itesm.mx>
2022-10-11 10:30:10 -0800

    Filtros eventos

--- app/src/main/java/com/example/qulturapp/view/eventos/EventoActivity.kt
+++ app/src/main/java/com/example/qulturapp/view/eventos/EventoActivity.kt
@@ -2,12 +2,14 @@ package com.example.qulturapp.view.eventos
 
 import android.os.Bundle
 import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.qulturapp.R
 import com.example.qulturapp.model.eventos.Evento
+import com.example.qulturapp.model.eventos.EventoLista
 import com.example.qulturapp.viewmodel.eventos.EventosListAdapter
 import com.example.qulturapp.viewmodel.eventos.EventosViewModel
 
@@ -15,6 +17,13 @@ class EventoActivity: AppCompatActivity() {
 
     private val eventosViewModel = EventosViewModel()
     private lateinit var adapter: EventosListAdapter
+    private lateinit var toggleTalleres: ToggleButton
+    private lateinit var toggleArtesEscenicas: ToggleButton
+    private lateinit var toggleMusica: ToggleButton
+    private lateinit var toggleDanza: ToggleButton
+    private lateinit var toggleAireLibre: ToggleButton
+    private lateinit var toggleCine: ToggleButton
+    private lateinit var toggleOtros: ToggleButton
 
     private fun mensajeErrorConexion() {
         Toast.makeText(applicationContext,"Ha ocurrido un error de conexión",
@@ -22,7 +31,7 @@ class EventoActivity: AppCompatActivity() {
             .show()
     }
 
-    private fun initializeList(list:List<Evento>) {
+    private fun initializeList(list:List<EventoLista>) {
         adapter = EventosListAdapter(list, this)
 
         val layoutManager = LinearLayoutManager(this)
@@ -32,20 +41,70 @@ class EventoActivity: AppCompatActivity() {
 
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_consulta_eventos)
+    private fun modificaFiltros(nombreTag: String, isChecked: Boolean) {
+        if(isChecked) {
+            eventosViewModel.filtrosTags.add(nombreTag)
+        } else {
+            eventosViewModel.filtrosTags.remove(nombreTag)
+        }
+        eventosViewModel.actualizaLista()
+    }
 
+    private fun initializeToggleListeners() {
+        toggleTalleres.setOnCheckedChangeListener { _, isChecked ->
+            modificaFiltros("Talleres", isChecked)
+        }
+        toggleArtesEscenicas.setOnCheckedChangeListener { _, isChecked ->
+            modificaFiltros("Artes Escénicas", isChecked)
+        }
+        toggleMusica.setOnCheckedChangeListener { _, isChecked ->
+            modificaFiltros("Música", isChecked)
+        }
+        toggleDanza.setOnCheckedChangeListener { _, isChecked ->
+            modificaFiltros("Danza", isChecked)
+        }
+        toggleAireLibre.setOnCheckedChangeListener { _, isChecked ->
+            modificaFiltros("Aire Libre", isChecked)
+        }
+        toggleCine.setOnCheckedChangeListener { _, isChecked ->
+            modificaFiltros("Cine", isChecked)
+        }
+        toggleOtros.setOnCheckedChangeListener { _, isChecked ->
+            modificaFiltros("Otros", isChecked)
+        }
+    }
+
+    private fun initializeObservers() {
         eventosViewModel.listaEventos.observe(this, Observer { list ->
-                if (list != null) {
-                    initializeList(list.eventos)
-                }
+            if (list != null) {
+                eventosViewModel.listaEventosFiltrados.postValue(list)
+            }
+        })
+        eventosViewModel.listaEventosFiltrados.observe(this, Observer { list ->
+            if (list != null) {
+                initializeList(list)
+            }
         })
         eventosViewModel.statusConexion.observe(this, Observer {
             if(it == false) {
                 mensajeErrorConexion()
             }
         })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consulta_eventos)
+        toggleTalleres = findViewById(R.id.filtro_talleres)
+        toggleArtesEscenicas = findViewById(R.id.filtro_artes_escenicas)
+        toggleMusica = findViewById(R.id.filtro_musica)
+        toggleDanza = findViewById(R.id.filtro_danza)
+        toggleAireLibre = findViewById(R.id.filtro_aire_libre)
+        toggleCine = findViewById(R.id.filtro_cine)
+        toggleOtros = findViewById(R.id.filtro_otros)
+
+        initializeObservers()
+        initializeToggleListeners()
 
         eventosViewModel.agregarEventos()
 

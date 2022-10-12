1cce8c93c4285ed41aa419391508d34ee2b01e59
LeilaelRico <A01707023@tec.mx>
2022-10-12 13:09:35 -0500

    Seekbar and Audio fully implemented.

--- app/src/main/java/com/example/qulturapp/view/artwork/Artwork.kt
+++ app/src/main/java/com/example/qulturapp/view/artwork/Artwork.kt
@@ -1,34 +1,32 @@
 package com.example.qulturapp.view.artwork
 
-import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageButton
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.qulturapp.R
 import com.example.qulturapp.model.artwork.ArtworkResults
-import com.example.qulturapp.view.museum.Lounge
 import com.example.qulturapp.viewmodel.artworks.ArtworkListAdapter
 import com.example.qulturapp.viewmodel.artworks.ArtworkViewModel
-import android.media.AudioManager
-import android.media.ImageReader
-import android.media.MediaPlayer
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Toast
 
 class Artwork : AppCompatActivity() {
 
-    lateinit var playIB: ImageButton
-    lateinit var stopIB: ImageButton
-    lateinit var mediaPlayer: MediaPlayer
-    //lateinit var play_pause: ImageView
-
     private lateinit var artList: List<ArtworkResults>
-
     private lateinit var adapter: ArtworkListAdapter
+    private lateinit var runnable: Runnable
 
+    private var handler = Handler()
     private val viewmodel: ArtworkViewModel by viewModels()
+
+    lateinit var playIB: ImageButton
+    lateinit var mediaPlayer: MediaPlayer
+    lateinit var seekbar: SeekBar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +35,7 @@ class Artwork : AppCompatActivity() {
         //viewmodel.getObra()
 
         playIB = findViewById(R.id.idIBPlay)
-        //stopIB = findViewById(R.id.idIBStop)
+        seekbar = findViewById(R.id.seekbar)
 
         mediaPlayer = MediaPlayer()
 
@@ -49,41 +47,44 @@ class Artwork : AppCompatActivity() {
 
         mediaPlayer.prepare()
 
+        seekbar.progress = 0
+        seekbar.max = mediaPlayer.duration
+
         playIB.setOnClickListener {
 
             if (mediaPlayer.isPlaying) {
 
                 playIB.setImageResource(R.drawable.ic_baseline_play_circle_outline_24)
                 mediaPlayer.pause()
-
                 Toast.makeText(applicationContext, "Audio se pausa", Toast.LENGTH_SHORT)
                     .show()
 
 
             } else {
 
-                    mediaPlayer.start()
-                    playIB.setImageResource(R.drawable.ic_baseline_pause_circle_outline_24)
+                mediaPlayer.start()
+                playIB.setImageResource(R.drawable.ic_baseline_pause_circle_outline_24)
 
                 Toast.makeText(applicationContext, "Audio inicia", Toast.LENGTH_SHORT)
                     .show()
 
-                }
+            }
+        }
+
+        runnable = Runnable {
+            seekbar.progress = mediaPlayer.currentPosition
+            handler.postDelayed(runnable,1000)
+        }
+
+        handler.postDelayed(runnable,1000)
+        mediaPlayer.setOnCompletionListener {
+            mediaPlayer.pause()
+            playIB.setImageResource(R.drawable.ic_baseline_play_circle_outline_24)
+
+
         }
 
 
-
-//        stopIB.setOnClickListener {
-//
-//                mediaPlayer.stop()
-//
-//                mediaPlayer.reset()
-//
-//                mediaPlayer.release()
-//
-//                Toast.makeText(applicationContext, "Audio se ha reiniciado", Toast.LENGTH_SHORT)
-//                    .show()
-//
-//        }
     }
+
 }

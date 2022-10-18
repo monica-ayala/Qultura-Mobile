7cf0fa6fe327caa33042331a976dd080e60f07b3
Jaime-prog <A00571842@tec.mx>
2022-10-18 14:35:52 -0500

    Interactive maps ui fixes

--- app/src/main/java/com/example/qulturapp/view/interactivo/ActivityInteractivoMAQRO.kt
+++ app/src/main/java/com/example/qulturapp/view/interactivo/ActivityInteractivoMAQRO.kt
@@ -1,16 +1,31 @@
 package com.example.qulturapp.view.interactivo
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.qulturapp.R
+import android.view.ScaleGestureDetector
+import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener
+import kotlin.math.max
+import kotlin.math.min
+
 
 class ActivityInteractivoMAQRO: AppCompatActivity() {
+    private lateinit var scaleGestureDetector: ScaleGestureDetector
+    private var scaleFactor = 1.0f
+    private lateinit var imageView: ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_interactivo_maqro)
+
+        //imageView = findViewById(R.id.imageViewMapMuseum)
+        //var scaleGestureDetector = ScaleGestureDetector(this, ScaleListener())
 
         var mapMuseum : ImageView = findViewById(R.id.imageViewMapMuseum)
         var floorChecker = 0
@@ -39,6 +54,8 @@ class ActivityInteractivoMAQRO: AppCompatActivity() {
         }
 
         btnPA.setOnClickListener {
+
+            btnPB.backgroundTintList
             floorChecker = 1
             mapMuseum.setImageResource(R.drawable.maqro_pa_vacio)
         }
@@ -50,6 +67,7 @@ class ActivityInteractivoMAQRO: AppCompatActivity() {
                     .show()
                 mapMuseum.setImageResource(R.drawable.maqro_pb_vacio)
             } else {
+
                 Toast.makeText(applicationContext,"No hay rampas en el segundo piso",
                     Toast.LENGTH_SHORT)
                     .show()
@@ -58,6 +76,8 @@ class ActivityInteractivoMAQRO: AppCompatActivity() {
         }
 
         btnElevator.setOnClickListener {
+
+
             if(floorChecker == 0) {
                 mapMuseum.setImageResource(R.drawable.maqro_pb_elevador)
             } else {
@@ -74,6 +94,7 @@ class ActivityInteractivoMAQRO: AppCompatActivity() {
         }
 
         btnReception.setOnClickListener {
+
             if(floorChecker == 0) {
                 mapMuseum.setImageResource(R.drawable.maqro_pb_recepcion)
             } else {
@@ -85,7 +106,9 @@ class ActivityInteractivoMAQRO: AppCompatActivity() {
         }
 
         btnOffice.setOnClickListener {
+
             if(floorChecker == 0) {
+                btnOffice.isEnabled= false
                 Toast.makeText(applicationContext,"Las oficinas se encuentran en el segundo piso",
                     Toast.LENGTH_SHORT)
                     .show()
@@ -93,9 +116,11 @@ class ActivityInteractivoMAQRO: AppCompatActivity() {
             } else {
                 mapMuseum.setImageResource(R.drawable.maqro_pa_oficinas)
             }
+            btnOffice.isEnabled = true
         }
 
         btnRestroom.setOnClickListener {
+
             if(floorChecker == 0) {
                 mapMuseum.setImageResource(R.drawable.maqro_pb_sanitarios)
             } else {
@@ -103,4 +128,26 @@ class ActivityInteractivoMAQRO: AppCompatActivity() {
             }
         }
     }
+
+     //   class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener{
+       //      fun onScale (ScaleGestureDetector detector): Boolean{
+         //       return true
+           // }
+        //}
+  //  override fun onTouchEvent(motionEvent: MotionEvent): Boolean {
+      //  scaleGestureDetector.onTouchEvent(motionEvent)
+      //  return true
+   // }
+
+   // private inner class ScaleListener : SimpleOnScaleGestureListener(){
+      //  override fun onScale(scaleGestureDetector: ScaleGestureDetector): Boolean{
+        //    scaleFactor *= scaleGestureDetector.scaleFactor
+        //    scaleFactor = max(0.1f, min(scaleFactor, 10.0f))
+        //    imageView.scaleX = scaleFactor
+      //      imageView.scaleY = scaleFactor
+
+      //      return true
+
+     //   }
+   // }
 }

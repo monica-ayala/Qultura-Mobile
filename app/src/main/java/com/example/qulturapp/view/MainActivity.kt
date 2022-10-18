a754368e6e735208b08fe89fc1ac7c6e87e4d4fe
saidortigoza <a01707430@itesm.mx>
2022-10-17 19:30:57 -0500

    Added MAQRO interactive map

--- app/src/main/java/com/example/qulturapp/view/MainActivity.kt
+++ app/src/main/java/com/example/qulturapp/view/MainActivity.kt
@@ -7,6 +7,7 @@ import com.example.qulturapp.R
 import com.example.qulturapp.model.database.DbUtil
 import com.example.qulturapp.view.interactivo.ActivityInteractivoCEART
 import com.example.qulturapp.view.interactivo.ActivityInteractivoGaleriasLibertad
+import com.example.qulturapp.view.interactivo.ActivityInteractivoMAQRO
 //import com.example.qulturapp.view.eventos.EventoActivity
 import com.example.qulturapp.view.interactivo.ActivityInterarctivo
 import com.example.qulturapp.view.museum.ListMuseum
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intentSignIn = Intent(this, ActivityInteractivoGaleriasLibertad::class.java)
+        val intentSignIn = Intent(this, ActivityInteractivoMAQRO::class.java)
         dbUtil.initRoomDatabase()
         try {
             dbUtil.initRoomDatabase()

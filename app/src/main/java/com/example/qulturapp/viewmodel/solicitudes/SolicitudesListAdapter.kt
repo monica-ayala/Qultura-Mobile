6ec5402f69e45687830bda247ed6a3901d097a78
EmilianoV-TEC <a01707035@itesm.mx>
2022-10-20 18:33:01 -0800

    fix img

--- app/src/main/java/com/example/qulturapp/viewmodel/solicitudes/SolicitudesListAdapter.kt
+++ app/src/main/java/com/example/qulturapp/viewmodel/solicitudes/SolicitudesListAdapter.kt
@@ -82,7 +82,7 @@ class ViewHolder (view: View): RecyclerView.ViewHolder(view) {
         horario.text = item.fecha
         asistentes.text = "Personas: " + item.asistentes.toString()
 
-        val ligaImg = "https://qulturaqro.live/uploads/" + item.imagen_museo
+        val ligaImg = "https://qulturaqro.live/uploads/museos/" + item.imagen_museo
         Picasso.get().load(ligaImg).into(imagenMuseo);
 
         var pos = true
